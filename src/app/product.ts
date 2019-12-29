c573e02543cb5346d5091afc29492b1a5ba7ed33
JT <coreprogramm@gmail.com>
2019-12-30 00:46:54 +0530

    Getting-Started-with-Angular-8-using-TypeScript
    Getting-Started-with-Angular-8-using-TypeScript

---
+++ src/app/product.ts
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Samsung',
+        price: 17799,
+        description: 'A large phone with one of the best screens'
+    },
+    {
+        name: 'Apple',
+        price: 36699,
+        description: 'A great phone with one of the best cameras'
+    },
+    {
+        name: 'Redmi',
+        price: 12299,
+        description: 'Triple Camera'
+    }
+];
